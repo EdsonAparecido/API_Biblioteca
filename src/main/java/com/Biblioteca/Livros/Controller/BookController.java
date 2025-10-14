1edd5914de77051ae07be01ee5c70e321edb3d88
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-14 14:21:41 -0300

    feat: criação de classe controller Book e adicionado os métodos com validação DTO

---
+++ src/main/java/com/Biblioteca/Livros/Controller/BookController.java
@@ -0,0 +1,49 @@
+package com.Biblioteca.Livros.Controller;
+
+import com.Biblioteca.Livros.DTO.BookCreateDTO;
+import com.Biblioteca.Livros.DTO.BookUpdateDTO;
+import com.Biblioteca.Livros.Model.Book;
+import com.Biblioteca.Livros.Service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Home/Book")
+public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> newBook(@Valid @RequestBody BookCreateDTO bookCreateDTO){
+        bookService.createBook(bookCreateDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public List<Book> readBook(){
+        return bookService.readBook();
+    }
+    @GetMapping("/{id}")
+    public Optional<Book> readBookById(@PathVariable Long id){
+        return bookService.readBookById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Void> updateBook(@PathVariable Long id,@Valid @RequestBody BookUpdateDTO bookUpdateDTO){
+        bookService.updateBook(id, bookUpdateDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+    }
+}
