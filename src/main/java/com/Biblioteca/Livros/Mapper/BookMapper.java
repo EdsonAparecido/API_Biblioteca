ab167145b97b9288eec2b3e6041121a3cb71f445
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-17 19:15:46 -0300

    feat: Adicionado no builder, o atributo 'status'

--- src/main/java/com/Biblioteca/Livros/Mapper/BookMapper.java
+++ src/main/java/com/Biblioteca/Livros/Mapper/BookMapper.java
@@ -1,14 +1,16 @@
 package com.Biblioteca.Livros.Mapper;
 
-import com.Biblioteca.Livros.DTO.BookCreateDTO;
-import com.Biblioteca.Livros.DTO.BookUpdateDTO;
+import com.Biblioteca.Livros.DTO.Book.BookCreateDTO;
+import com.Biblioteca.Livros.DTO.Book.BookUpdateDTO;
 import com.Biblioteca.Livros.Model.Book;
+import com.Biblioteca.Livros.Model.TypeStatus;
 
 public class BookMapper {
     public static Book DTOCreateToBook(BookCreateDTO bookCreateDTO){
         return Book.builder()
                 .name(bookCreateDTO.getName())
                 .autor(bookCreateDTO.getAutor())
+                .status(TypeStatus.AVAILABLE)
                 .build();
     }
     public static Book DTOUpdateToBook(BookUpdateDTO bookUpdateDTO){
