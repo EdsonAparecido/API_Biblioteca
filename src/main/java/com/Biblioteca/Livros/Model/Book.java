f956e80df04c225ac3cee417c389a0eaec928fcd
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-17 19:13:12 -0300

    feat: adicionado tipo de estado do livro: 'disponível', 'indisponível'

--- src/main/java/com/Biblioteca/Livros/Model/Book.java
+++ src/main/java/com/Biblioteca/Livros/Model/Book.java
@@ -24,7 +24,7 @@ public class Book {
     @Column(nullable = false)
     private String autor;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Enumerated(EnumType.STRING)
+    private TypeStatus status;
+
 }
