821415bcbc415548a005af98b16f3afea03f28a9
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-17 19:18:30 -0300

    feat: Adicionado model Emprestimo, com FKS das tabelas

---
+++ src/main/java/com/Biblioteca/Livros/Model/Emprestimo.java
@@ -0,0 +1,29 @@
+package com.Biblioteca.Livros.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_Emprestimo")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Emprestimo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @ManyToOne
+    @JoinColumn
+    private Book book;
+
+}
