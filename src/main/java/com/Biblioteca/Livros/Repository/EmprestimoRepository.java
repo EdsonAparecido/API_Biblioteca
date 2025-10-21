bd4d6e00b388cd949e37f85f439518aade57fc06
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-21 11:47:54 -0300

    feat: add retorno de emprestimo se existir um emprestimo com Book e Devolucao null

--- src/main/java/com/Biblioteca/Livros/Repository/EmprestimoRepository.java
+++ src/main/java/com/Biblioteca/Livros/Repository/EmprestimoRepository.java
@@ -1,8 +1,12 @@
 package com.Biblioteca.Livros.Repository;
+import com.Biblioteca.Livros.Model.Book;
 import com.Biblioteca.Livros.Model.Emprestimo;
+import com.Biblioteca.Livros.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{ }
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{
+    Optional<Emprestimo> findByBookAndDataDevolucaoIsNull(Book book);
+}
