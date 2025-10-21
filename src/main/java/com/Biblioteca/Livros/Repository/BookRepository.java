db5209172faa6c2354bbe19aa3d8659beb954d73
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-21 11:46:36 -0300

    refactor: alterado nome do package e TypeStatus -> TypeStatusBook

--- src/main/java/com/Biblioteca/Livros/Repository/BookRepository.java
+++ src/main/java/com/Biblioteca/Livros/Repository/BookRepository.java
@@ -1,10 +1,10 @@
 package com.Biblioteca.Livros.Repository;
 import com.Biblioteca.Livros.Model.Book;
-import com.Biblioteca.Livros.Model.TypeStatus;
+import com.Biblioteca.Livros.Model.Enum.TypeStatusBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
-    Optional<Boolean> existsByIdAndStatus(Long id, TypeStatus typeStatus);
+    Optional<Boolean> existsByIdAndStatus(Long id, TypeStatusBook typeStatusBook);
 }
