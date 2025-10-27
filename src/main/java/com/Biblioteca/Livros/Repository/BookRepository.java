9d3c217ad02f6d16a9256bc1cf7ddd9e66b5cf52
EdsonAparecido <edsonsilvaaparecido6@gmail.com>
2025-10-27 20:31:42 -0300

    refactor: nome dos métodos SQL

--- src/main/java/com/Biblioteca/Livros/Repository/BookRepository.java
+++ src/main/java/com/Biblioteca/Livros/Repository/BookRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
-    Optional<Boolean> existsByIdAndStatus(Long id, TypeStatusBook typeStatusBook);
+    Optional<Boolean> existsByIdAndStatus(Long id, TypeStatusBook status);
 }
--- src/main/java/com/Biblioteca/Livros/Repository/UserRepository.java
+++ src/main/java/com/Biblioteca/Livros/Repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByIdAndBookIsNull(Long id);
-    Optional<Boolean> existsByIdAndBook(Long idUser, Book book);
+    Optional<Boolean> existsByIdAndBook(Long id, Book book);
 }
 
 
